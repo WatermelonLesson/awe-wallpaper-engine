bb3259569d28a066775713c32489b7107e1498e3
Nathaniel Rodriguez <njr+git@nathanielrodriguez.me>
2021-03-15 07:43:29 -0400

    Redesign
    Refactor with nicer design.

---
+++ js/form_controls.js
@@ -0,0 +1,32 @@
+document.querySelectorAll("input").forEach(el => {
+	el.addEventListener("input", function(e) {
+		if (e.target.id === "bg_width") {
+			CANVAS_ELEMENT.width = e.target.value;
+		}
+		else if (e.target.id === "bg_height") {
+			console.log("Height");
+			CANVAS_ELEMENT.height = e.target.value;
+		}
+		else if (e.target.id === "num_rectangles") {
+			console.log("Number of rectangles: ");
+			num_rect = e.target.value;
+		}
+		else if (e.target.id === "num_arcs") {
+			console.log("Number of arcs");
+			num_arcs = e.target.value;
+		}
+		else if (e.target.id === "bg_color") {
+			console.log("Background color");
+			document.querySelector("main").style.backgroundColor = e.target.value;
+		}
+		else if (e.target.id === "animation_checkbox") {
+			if (e.target.checked) {
+				console.log("Animation checkbox");
+				animation_interval = setInterval(resetBackground, document.querySelector("input[type=range]#animation_range").value);
+			}
+			else {
+				clearInterval(animation_interval);
+			}
+		}
+	});
+});
